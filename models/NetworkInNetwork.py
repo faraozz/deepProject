10fbcb96d03586f493ec225a44906cba92f912e2
Paul Schlumbom <paul.schlumbom@gmail.com>
2021-05-11 20:49:33 +1200

    added file for NiN implementation (incomplete)

---
+++ models/NetworkInNetwork.py
@@ -0,0 +1,9 @@
+#
+# Network in network implementation
+# original paper: https://arxiv.org/pdf/1312.4400.pdf%20http://arxiv.org/abs/1312.4400.pdf
+#
+
+import numpy as np
+import torch
+from torch import nn
+
