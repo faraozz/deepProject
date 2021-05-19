d50f1f28942ea4a586b213fbc0975adfd094bca7
Paul Schlumbom <paul.schlumbom@gmail.com>
2021-05-19 18:29:52 +1200

    streaming implemented (untested)

--- training/streaming.py
+++ training/streaming.py
@@ -8,6 +8,7 @@ import torch
 from os import listdir
 from os.path import join
 from training.dataset import Dataset
+from models.NetworkInNetwork import NiN
 
 # CUDA for torch
 use_cuda = False  # for now, avoid GPU stuff
@@ -15,12 +16,16 @@ device = torch.device("cpu")
 torch.backends.cudnn.benchmark = True
 
 # parameters
+model_name = "test"  # name to save model under
 max_epochs = 10
 params = {
     'batch_size': 64,
     'shuffle': False,  # already shuffled
     'num_workers': 2
 }
+model = NiN(1, 4)  # 3 input channel (colour images), 4 output channels (= 4 outputs)
+optimizer = torch.optim.SGD(model.parameters(), lr=0.001)
+criterion = torch.nn.CrossEntropyLoss()
 
 # Datasets
 trainpath = "../data/traindata.npz"
@@ -41,10 +46,18 @@ validation_generator = torch.utils.data.Dataloader(validation_set, **params)
 
 # loop over epochs
 for epoch in range(max_epochs):
+    train_loss = None
+    val_loss = None
     for local_batch, local_labels in training_generator:
         local_batch, local_labels = local_batch.to(device), local_labels.to(device)
 
         # Model computations
+        optimizer.zero_grad()
+        pred = model(local_batch)
+        loss = criterion(pred, local_labels)
+        loss.backward()
+        optimizer.step()
+        train_loss = loss.data[0]
 
     # Validation
     with torch.set_grad_enabled(False):
@@ -52,4 +65,13 @@ for epoch in range(max_epochs):
             local_batch, local_labels = local_batch.to(device), local_labels.to(device)
 
             # Model computations
+            optimizer.zero_grad()
+            pred = model(local_batch)
+            loss = criterion(pred, local_labels)
+            val_loss = loss.data[0]
+
+    print("Epoch {}: train_loss = {}, val_loss = {}".format(epoch, train_loss, val_loss))
+
+# Save model
+torch.save(model.state_dict(), join("../models/checkpoints", model_name))
 
