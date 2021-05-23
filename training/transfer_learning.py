dd8c1548d4c9b01de4686faa989c3f84faec7267
Paul Schlumbom <paul.schlumbom@gmail.com>
2021-05-23 16:28:28 +1200

    code cleaded up

--- training/transfer_learning.py
+++ training/transfer_learning.py
@@ -21,6 +21,15 @@ NUMBER_OF_CLASSES = 10
 #
 
 def load_model(modeltype, modelpath, trained_model_classes, new_model_classes=None):
+    """
+    Load a model of a particular type and load stored parameters
+    :param modeltype: torch object, the neural network object or which parameters are loaded
+    :param modelpath: string, path of the model parameters
+    :param trained_model_classes: the number of output classes of the trained model being loaded
+    :param new_model_classes: optional scalar, the number of output parameters in the new model (replacing the
+    original output layer)
+    :return: torch object, a neural network model with the parameters loaded
+    """
     if modeltype == "alexnet":
         model = torch.hub.load('pytorch/vision:v0.6.0', 'alexnet', pretrained=False)
         model.features[0] = nn.Conv2d(1, 64, kernel_size=(11, 11), stride=1, padding=2)
@@ -42,12 +51,18 @@ def load_model(modeltype, modelpath, trained_model_classes, new_model_classes=No
 # freeze all except final n layers
 #
 
-def freeze_last_layers(model, layers=1):
+def freeze_last_layers(model, n_free=1):
+    """
+    Given a model, freeze all except the last n_free layers so that only these last layers can be trained
+    :param model: torch object, a neural network model (preferably with trained parameters)
+    :param n_free: int, the number of last layers to free for training
+    :return: torch object, the neural network model with the last n_free layers free for training
+    """
     for param in model.parameters():
         param.requires_grad = False
 
     num_layers = len(model.features)
-    for l in range(num_layers - layers, num_layers):
+    for l in range(num_layers - n_free, num_layers):
         for param in model.features[l].parameters():
             param.requires_grad = True
 
